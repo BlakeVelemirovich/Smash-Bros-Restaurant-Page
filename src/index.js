9333571774114fd36836819679d2529d4250b682
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-02-04 20:54:17 -0400

    Created seperate div containors for styling purposes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 // Header title Creation
 let homeTitle = document.createElement('h1');
 let titleText = document.createTextNode('Super Smash Brothers!');
-
 homeTitle.appendChild(titleText);
 
+let headerContainer = document.createElement('div');
+let buttonContainer = document.createElement('div');
+headerContainer.className = 'header';
+buttonContainer.className = 'headerButtons';
+headerContainer.appendChild(homeTitle);
+
 let contentSelector = document.querySelector('#content');
-contentSelector.appendChild(homeTitle);
+contentSelector.appendChild(headerContainer);
+contentSelector.appendChild(buttonContainer);
 
 // Header buttons function
 let createButtons = function(name) {
@@ -13,7 +19,7 @@ let createButtons = function(name) {
     let newButton = document.createElement('button');
     let text = document.createTextNode(name);
     newButton.appendChild(text);
-    contentSelector.appendChild(newButton);
+    buttonContainer.appendChild(newButton);
 }
 
 window.onload = () => {
