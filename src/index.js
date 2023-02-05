4bc94f0ffc26b180e34a430d6a81d3bef5b35d83
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-02-04 21:37:15 -0400

    Content now has paragraph and images

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ let contentSelector = document.querySelector('#content');
 contentSelector.appendChild(headerContainer);
 contentSelector.appendChild(buttonContainer);
 
-// Header buttons function
+// Header buttons factory function and creation
 let createButtons = function(name) {
 
     let newButton = document.createElement('button');
@@ -30,4 +30,16 @@ window.onload = () => {
 }
 
 //Content 
+let contentContainer = document.createElement('div');
+contentContainer.className = 'contentContainer';
+contentSelector.appendChild(contentContainer);
+
+let contentP = document.createElement('p');
+let contentText = document.createTextNode('Just a little information about the characters I play and smash and my smash links');
+contentP.appendChild(contentText);
+contentContainer.appendChild(contentP);
+
+let contentImage = document.createElement('img');
+contentImage.src = '../images/Smash.png';
+contentContainer.appendChild(contentImage);
 
